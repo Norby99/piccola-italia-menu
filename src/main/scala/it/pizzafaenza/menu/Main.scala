013a3ad5b6b9120f46298828b159300be187e036
Norby99 <gabos.norbert99@gmail.com>
2025-12-16 16:45:40 +0100

    create basic pizza cell

--- src/main/scala/it/pizzafaenza/menu/Main.scala
+++ src/main/scala/it/pizzafaenza/menu/Main.scala
@@ -2,18 +2,45 @@ package it.pizzafaenza.menu
 
 import com.raquo.laminar.api.L.*
 import org.scalajs.dom
+import org.scalajs.dom.window
+import it.pizzafaenza.menu.Ingredienti.IngredientCollection
+import it.pizzafaenza.menu.pizze.{Pizza, PizzeCollection}
+import it.pizzafaenza.menu.json.BrowserJsonReader
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import it.pizzafaenza.menu.UIElements.PizzaCellRenderer
+
+def text_resizer(n: Float, ratio: Float): Float = n / (2550 / ratio)
 
 @main def runApp(): Unit =
+  val windowWidth = Var(window.outerWidth)
+
+  window.addEventListener(
+    "resize",
+    { (_: dom.Event) =>
+      windowWidth.set(window.outerWidth)
+    }
+  )
+
+  val ingredientiFuture = IngredientCollection(BrowserJsonReader).getIngredients
+
+  val pizzeFuture = ingredientiFuture.flatMap { ingrList =>
+    PizzeCollection(BrowserJsonReader).getPizze(ingrList)
+  }
+
+  val pizzeVar = Var(List.empty[Pizza])
+
+  pizzeFuture.foreach { pizze =>
+    pizzeVar.set(pizze)
+  }
+
   val app = div(
-    navTag(
-      a(href := "#home", "Home"),
-      " | ",
-      a(href := "#about", "About")
-    ),
-    hr(),
-    child <-- Var("Benvenuto nella mini SPA Laminar!").signal.map(text =>
-      p(text)
-    )
+    h1("Menu Pizzeria Faenza"),
+    h2("Le nostre pizze"),
+    children <-- pizzeVar.signal.map { pizze =>
+      pizze.map { pizza =>
+        PizzaCellRenderer(pizza).render
+      }
+    }
   )
 
   renderOnDomContentLoaded(dom.document.getElementById("app"), app)
