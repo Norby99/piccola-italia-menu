ec0f038047164ed6e5d2c417a096397cf33e35f2
Norbert Gabos <dont.vip@gmail.com>
2026-01-14 14:33:11 +0100

    Add 'Stese' category to PizzaCategory enum

--- src/main/scala/it/pizzafaenza/menu/pizze/PizzaCategory.scala
+++ src/main/scala/it/pizzafaenza/menu/pizze/PizzaCategory.scala
@@ -4,3 +4,4 @@ enum PizzaCategory(val description: String):
   case Classiche extends PizzaCategory("Pizze classiche")
   case Bianche extends PizzaCategory("Pizze bianche")
   case Conditissime extends PizzaCategory("Pizze conditissime")
+  case Stese extends PizzaCategory("Pizze stese")
