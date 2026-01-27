549c98dda7a41564aaa1d0c2fdc7abcc8a05e6bd
Norbert Gabos <dont.vip@gmail.com>
2026-01-27 16:41:34 +0100

    Introduce DishCategory trait and unify category handling

--- src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
+++ src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
@@ -3,5 +3,9 @@ package it.pizzafaenza.menu.salads
 import it.pizzafaenza.menu.ingredients.Ingredient
 import it.pizzafaenza.menu.menu.MenuDish
 
-case class Salad(name: String, ingredients: List[Ingredient], price: Double)
-    extends MenuDish
+case class Salad(
+    name: String,
+    category: SaladCategory,
+    ingredients: List[Ingredient],
+    price: Double
+) extends MenuDish
