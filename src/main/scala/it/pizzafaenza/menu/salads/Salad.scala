002a6c81ffa8bc494d982e1d86f52c8e08f03af8
Norbert Gabos <dont.vip@gmail.com>
2026-01-27 15:44:06 +0100

    Add Salad and SaladCollection with tests

--- src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
+++ src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
@@ -1,11 +1,5 @@
 package it.pizzafaenza.menu.salads
 
 import it.pizzafaenza.menu.ingredients.Ingredient
-import it.pizzafaenza.menu.menu.{MenuDish, SaladCategory}
 
-case class Salad(
-    name: String,
-    category: SaladCategory,
-    ingredients: List[Ingredient],
-    price: Double
-) extends MenuDish
+case class Salad(name: String, ingredients: List[Ingredient], price: Double)
--- src/main/scala/it/pizzafaenza/menu/salads/SaladCollection.scala
+++ src/main/scala/it/pizzafaenza/menu/salads/SaladCollection.scala
@@ -3,27 +3,17 @@ package it.pizzafaenza.menu.salads
 import it.pizzafaenza.menu.json.JsonReader
 import io.circe.*
 import it.pizzafaenza.menu.ingredients.Ingredient
-import it.pizzafaenza.menu.menu.{MenuDishCollection, SaladCategory}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class SaladCollection(jsonReader: JsonReader)(implicit ec: ExecutionContext)
-    extends MenuDishCollection[Salad](
-      jsonReader,
-      "data/insalate.json",
-      SaladCollection.saladDecoder
-    ):
+class SaladCollection(jsonReader: JsonReader)(implicit ec: ExecutionContext):
+  private val DBPath = "data/insalate.json"
 
-  def getSalad(ingredients: List[Ingredient]): Future[List[Salad]] =
-    getDish(ingredients)
-
-object SaladCollection:
   private def saladDecoder(ingredients: List[Ingredient]): Decoder[Salad] =
     new Decoder[Salad]:
       def apply(c: HCursor): Decoder.Result[Salad] =
         for
           name <- c.downField("nomeInsalata").as[String]
-          category <- Right(SaladCategory.Salad)
           ingr <-
             val ingIds: List[Int] =
               c.downField("ingredienti").as[String].map(
@@ -39,4 +29,30 @@ object SaladCollection:
                   )
                 )
           price <- c.downField("prezzo").as[String].map(_.toDouble)
-        yield Salad(name, category, ingr, price)
+        yield Salad(name, ingr, price)
+
+  def getSalad(ingredients: List[Ingredient]): Future[List[Salad]] =
+    jsonReader.read(DBPath).map { content =>
+      given Decoder[Salad] = saladDecoder(ingredients)
+
+      println("Decoding...")
+      io.circe.parser.parse(content.noSpaces) match
+        case Right(json) =>
+          val res = json.asArray match
+            case Some(saladArray) =>
+              saladArray.flatMap { saladJson =>
+                saladJson.as[Salad] match
+                  case Right(salad) =>
+                    println("---:" + salad)
+                    Some(salad)
+                  case Left(error) =>
+                    println(s"Skipping salad: ${error.getMessage}")
+                    None
+              }.toList
+            case None =>
+              throw new Exception("JSON is not an array")
+          println("Decoded salad: " + res.size)
+          res
+        case Left(error) =>
+          throw new Exception(s"Failed to parse JSON: $error")
+    }
