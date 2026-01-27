4c839111dbf0c47483d506e3f0508de8541b83c0
Norbert Gabos <dont.vip@gmail.com>
2026-01-27 16:00:32 +0100

    Introduce MenuDish trait and unify dish types

--- src/main/scala/it/pizzafaenza/menu/pizza/Pizza.scala
+++ src/main/scala/it/pizzafaenza/menu/pizza/Pizza.scala
@@ -1,10 +1,11 @@
 package it.pizzafaenza.menu.pizza
 
 import it.pizzafaenza.menu.ingredients.Ingredient
+import it.pizzafaenza.menu.menu.MenuDish
 
 case class Pizza(
     name: String,
     category: PizzaCategory,
     ingredients: List[Ingredient],
     price: Double
-)
+) extends MenuDish
--- src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
+++ src/main/scala/it/pizzafaenza/menu/salads/Salad.scala
@@ -1,5 +1,7 @@
 package it.pizzafaenza.menu.salads
 
 import it.pizzafaenza.menu.ingredients.Ingredient
+import it.pizzafaenza.menu.menu.MenuDish
 
 case class Salad(name: String, ingredients: List[Ingredient], price: Double)
+    extends MenuDish
