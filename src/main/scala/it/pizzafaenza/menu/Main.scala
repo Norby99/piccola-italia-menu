0bbb69280f00f15d98d91e5e455bac7dd149f4fa
Norbert Gabos <dont.vip@gmail.com>
2026-01-27 16:08:49 +0100

    Refactor futures for ingredients and pizze

--- src/main/scala/it/pizzafaenza/menu/Main.scala
+++ src/main/scala/it/pizzafaenza/menu/Main.scala
@@ -1,14 +1,23 @@
 package it.pizzafaenza.menu
 
-import it.pizzafaenza.menu.ingredients.IngredientCollection
+import it.pizzafaenza.menu.ingredients.{Ingredient, IngredientCollection}
 import it.pizzafaenza.menu.pizza.{Pizza, PizzeCollection}
 import it.pizzafaenza.menu.menu.Menu
 import it.pizzafaenza.menu.json.BrowserJsonReader
-
 import com.raquo.laminar.api.L.*
 import org.scalajs.dom
 import org.scalajs.dom.window
+
+import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+def ingredientsFuture: Future[List[Ingredient]] =
+  IngredientCollection(BrowserJsonReader).getIngredients
+
+def pizzeFuture: Future[List[Pizza]] =
+  ingredientsFuture.flatMap { ing =>
+    PizzeCollection(BrowserJsonReader).getPizze(ing)
+  }
 
 @main def runApp(): Unit =
   val windowWidth = Var(window.outerWidth)
@@ -20,14 +29,7 @@ import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
     }
   )
 
-  val ingredientsFuture = IngredientCollection(BrowserJsonReader).getIngredients
-
-  val pizzeFuture = ingredientsFuture.flatMap { ing =>
-    PizzeCollection(BrowserJsonReader).getPizze(ing)
-  }
-
   val pizzeVar = Var(List.empty[Pizza])
-
   pizzeFuture.foreach { pizze =>
     pizzeVar.set(pizze)
   }
