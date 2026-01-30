7871884bbd29662fa868a597350d84d1b65ac53d
Norbert Gabos <dont.vip@gmail.com>
2026-01-30 14:20:22 +0100

    Replace MockGamberetti with shared mock

--- src/test/scala/it/pizzafaenza/menu/ingredients/IngredientCollectionTest.scala
+++ src/test/scala/it/pizzafaenza/menu/ingredients/IngredientCollectionTest.scala
@@ -1,7 +1,7 @@
 package it.pizzafaenza.menu.ingredients
 
 import it.pizzafaenza.menu.ingredients.{Ingredient, IngredientCollection}
-import it.pizzafaenza.menu.mock.MockGamberettiReader
+import it.pizzafaenza.menu.mock.MockIngredientsReader
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -10,14 +10,14 @@ import scala.concurrent.Future
 class IngredientCollectionTest extends AsyncFlatSpec with Matchers:
 
   "IngredientCollection" should "load gamberetti from a JSON" in:
-    val collection = new IngredientCollection(MockGamberettiReader)
-    val ingredients: Future[List[Ingredient]] = collection.getIngredients
+    val collection = new IngredientCollection(MockIngredientsReader)
 
-    ingredients.map { i =>
-      i.length shouldBe 1
-      i.head.id shouldBe 50
-      i.head.name.italian shouldBe "gamberetti"
-      i.head.name.english shouldBe "shrimps"
-      i.head.allergen.name shouldBe "Pesce"
+    for
+      ingredients <- collection.getIngredients
+      gamberetti = ingredients.find(_.id == 50).get
+    yield
+      gamberetti.id shouldBe 50
+      gamberetti.name.italian shouldBe "gamberetti"
+      gamberetti.name.english shouldBe "shrimps"
+      gamberetti.allergen.name shouldBe "Pesce"
       succeed
-    }
