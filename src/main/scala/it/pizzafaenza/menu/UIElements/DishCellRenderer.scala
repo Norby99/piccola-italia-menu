33d111899dfaed56bf3e2ee214a4e36ece18bfb1
Norbert Gabos <dont.vip@gmail.com>
2026-01-30 15:33:35 +0100

    Rename PizzaCellRenderer to DishCellRenderer

--- src/main/scala/it/pizzafaenza/menu/UIElements/DishCellRenderer.scala
+++ src/main/scala/it/pizzafaenza/menu/UIElements/DishCellRenderer.scala
@@ -4,13 +4,18 @@ import com.raquo.laminar.api.L.*
 import it.pizzafaenza.menu.menu.MenuDish
 import it.pizzafaenza.menu.utils.stringify
 
-class DishCellRenderer(dish: MenuDish)
+class DishCellRenderer(dish: MenuDish, rowCount: Int)
     extends CellRenderer:
   @Override
-  def render(heightProportion: Int): Div =
+  def render: Div =
     div(
       cls := "pizza-cell",
-      styleAttr := s"--elements-per-column: $heightProportion",
+      onMountCallback(ctx =>
+        ctx.thisNode.ref.style.setProperty(
+          "--elements-per-column",
+          rowCount.toString
+        )
+      ),
       div(
         cls := "pizza-header",
         p(
