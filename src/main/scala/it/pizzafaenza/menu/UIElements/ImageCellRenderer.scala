0179cb81c678eec26d53c88b4d175969ee917f52
Norbert Gabos <gabos.norbert99@gmail.com>
2026-02-15 13:14:50 +0100

    refactor: LogoCell rename to ImageCell

--- src/main/scala/it/pizzafaenza/menu/UIElements/ImageCellRenderer.scala
+++ src/main/scala/it/pizzafaenza/menu/UIElements/ImageCellRenderer.scala
@@ -2,11 +2,10 @@ package it.pizzafaenza.menu.UIElements
 
 import com.raquo.laminar.api.L.*
 
-class ImageCellRenderer(imagePath: String)
-    extends CellRenderer:
+class ImageCellRenderer(imagePath: String) extends CellRenderer:
   @Override
   def render(heightProportion: Int = 0): Div =
     div(
-      cls := "pizza-cell square-cell",
+      cls := "pizza-cell logo-cell",
       img(src := imagePath, alt := "Missing Image")
     )
