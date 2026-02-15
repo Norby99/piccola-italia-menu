8bef2c9b84235bd3d203df74e915e040abcba6bb
Norbert Gabos <gabos.norbert99@gmail.com>
2026-02-15 13:53:40 +0100

    chore: removed usless code

--- src/main/scala/it/pizzafaenza/menu/UIElements/ImageCellRenderer.scala
+++ src/main/scala/it/pizzafaenza/menu/UIElements/ImageCellRenderer.scala
@@ -2,11 +2,11 @@ package it.pizzafaenza.menu.UIElements
 
 import com.raquo.laminar.api.L.*
 
-class ImageCellRenderer(imagePath: String, inGrid: Boolean = true)
+class ImageCellRenderer(imagePath: String)
     extends CellRenderer:
   @Override
   def render(heightProportion: Int = 0): Div =
     div(
-      cls := (if inGrid then "pizza-cell square-cell" else "square-cell"),
+      cls := "pizza-cell square-cell",
       img(src := imagePath, alt := "Missing Image")
     )
