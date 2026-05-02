79fcd5fb5a3450203715bbef8693dd2f09176495
Norby99 <gabos.norbert99@gmail.com>
2026-05-02 13:20:04 +0200

    test: create mock for allergens

---
+++ src/test/scala/it/pizzafaenza/menu/mock/MockAllergensReader.scala
@@ -0,0 +1,31 @@
+package it.pizzafaenza.menu.mock
+
+import io.circe.Json
+import it.pizzafaenza.menu.json.JsonReader
+import scala.concurrent.Future
+
+import scala.concurrent.Future
+
+object MockAllergensReader extends JsonReader:
+  override def read(path: String): Future[Json] =
+    val mockJsonString =
+      """
+        |[
+        |   {
+        |       "id":"1",
+        |       "tipo":"Null"
+        |   },
+        |   {
+        |       "id":"2",
+        |       "tipo":"Uova"
+        |   },
+        |   {
+        |       "id":"3",
+        |       "tipo":"Pesce"
+        |   }
+        |]
+        |""".stripMargin
+
+    Future.successful(
+      io.circe.parser.parse(mockJsonString).getOrElse(Json.Null)
+    )
