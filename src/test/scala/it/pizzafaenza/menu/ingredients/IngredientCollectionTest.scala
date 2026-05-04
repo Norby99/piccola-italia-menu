7cf874c83bed85e2aeb2a0cdc8ecc4a212d6c61b
Norby99 <gabos.norbert99@gmail.com>
2026-05-04 13:22:31 +0200

    test: implement allergens in pizza, salad and ingredient

--- src/test/scala/it/pizzafaenza/menu/ingredients/IngredientCollectionTest.scala
+++ src/test/scala/it/pizzafaenza/menu/ingredients/IngredientCollectionTest.scala
@@ -1,6 +1,7 @@
 package it.pizzafaenza.menu.ingredients
 
-import it.pizzafaenza.menu.mock.MockIngredientsReader
+import it.pizzafaenza.menu.allergens.AllergenCollection
+import it.pizzafaenza.menu.mock.{MockAllergensReader, MockIngredientsReader}
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -12,7 +13,8 @@ class IngredientCollectionTest extends AsyncFlatSpec with Matchers:
     val collection = new IngredientCollection(MockIngredientsReader)
 
     for
-      ingredients <- collection.getIngredients
+      allergens <- AllergenCollection(MockAllergensReader).getAllergens
+      ingredients <- collection.getIngredients(allergens)
       gamberetti = ingredients.find(_.id == 50).get
     yield
       gamberetti.id shouldBe 50
